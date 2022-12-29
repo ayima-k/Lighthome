7342dbf5639ab5e2452e0c6e58b3a18c1c566cfd
Ajyma <ajymakockorova@gmail.com>
2022-12-29 23:16:43 +0600

    updated

--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import './Contacts.scss'
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+    <div className='contacts'>
+      <h1>Наш адрес</h1>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d758.194374420584!2d72.76978037735259!3d40.52440881871228!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skg!4v1672333695603!5m2!1sru!2skg" width='100%' height='500' style={{border: 0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    </div>
   )
 }
 
